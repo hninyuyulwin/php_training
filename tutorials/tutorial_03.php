ed36978a0353070226039c3182fe6e184d09a0e7
Rosie Mary <laltnadi@gmail.com>
2022-07-21 17:10:41 +0630

    Tuto-03 Review Checked

--- tutorials/tutorial_03.php
+++ tutorials/tutorial_03.php
@@ -13,7 +13,7 @@
     </style>
   </head>
   <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
       Enter your birth date and I will calculate your age : <br><br>
       <input class="input-date" type="date" name="bd"><br><br>
       <input type="submit" value="Calculate">
@@ -21,14 +21,14 @@
   </body>
 </html>
 <?php
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $userBD = $_POST['bd'];
     $nowYear = date('Y');
-    if (date('Y',strtotime($userBD)) > date('Y')) {
-      echo "<h3>Your birthday is invalid</h3>";
-    }else{
-      $diff = ($nowYear - date('Y',strtotime($userBD)));
-      echo "<h3>Your age is $diff years old.</h3>";
+    if (date('Y', strtotime($userBD)) > date('Y')) {
+        echo "<h3>Your birthday is invalid</h3>";
+    } else {
+        $diff = ($nowYear - date('Y', strtotime($userBD)));
+        echo "<h3>Your age is $diff years old.</h3>";
     }
-  }
+}
 ?>
