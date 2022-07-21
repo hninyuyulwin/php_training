701839bdd479884af421c2bd5b71af44a1a3600f
Rosie Mary <laltnadi@gmail.com>
2022-07-21 17:04:27 +0630

    Tuto-03 Review Checked

--- tutorials/tutorial_03.php
+++ tutorials/tutorial_03.php
@@ -1,11 +1,21 @@
 <html>
   <head>
     <title>Age | Calculation</title>
+    <style>
+      .input-date {
+        width : 15%;
+        outline : none;
+      }
+      form {
+        margin: 50px auto;
+        padding-left : 30%;
+      }
+    </style>
   </head>
   <body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
       Enter your birth date and I will calculate your age : <br><br>
-      <input type="text" name="bd"><br><br>
+      <input class="input-date" type="date" name="bd"><br><br>
       <input type="submit" value="Calculate">
     </form>
   </body>
