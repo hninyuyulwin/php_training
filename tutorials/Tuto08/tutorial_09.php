33bafb7e0c9cfbbccd9af837e79eca2370b06310
Rosie Mary <laltnadi@gmail.com>
2022-07-26 14:25:10 +0630

    Adding Tutorial_09 file chart

---
+++ tutorials/Tuto08/tutorial_09.php
@@ -0,0 +1,66 @@
+<?php
+    require_once "common/config.php";
+
+    $stmt = $conn->prepare("SELECT * FROM alluser");
+    $stmt->execute(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pie Chart</title>
+    
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.css" rel="stylesheet"/>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['User-Name', 'User-Age'],
+          <?php
+            while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+                echo "['".$result['username']."',".$result['age']."],"; 
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Showing User Age with Pie Chart'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+</head>
+<body class="bg-dark">
+    <div class="row">
+        <div class="col-lg-9 m-auto mt-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Showing User Age Pie Chart</h3>
+                </div>
+                <div class="card-body">
+                    <div id="piechart" style="width: 900px; height: 500px;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.js"></script>
+</body>
+</html>
